216ed05cf73d74b799e6ccbaaa9c53f9ac72aaa0
Purushotham <purushotham@hasura.io>
2021-08-11 12:58:05 +0530

    init commit

--- cli-check/cmd/main.go
+++ cli-check/cmd/main.go
@@ -6,5 +6,6 @@ import (
 
 // main is the entrypoint function
 func main() {
-	fmt.Println("this is a test binary")
+	fmt.Println("this is a test binary executable")
+	fmt.Println("Let's see how github action runs")
 }
